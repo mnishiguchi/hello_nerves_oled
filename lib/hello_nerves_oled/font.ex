836b79a993793197e499f382c759f085bde2d1db
Masatoshi N <nishiguchi.masa@gmail.com>
2022-05-28 14:15:27 -0400

    Add ChiselFontCache

--- lib/hello_nerves_oled/font.ex
+++ lib/hello_nerves_oled/font.ex
@@ -1,6 +1,8 @@
 defmodule HelloNervesOled.Font do
   @moduledoc false
 
+  alias HelloNervesOled.ChiselFontCache
+
   @fonts_destination_dir "/data/fonts"
   @fonts_source_base_url "https://raw.githubusercontent.com/olikraus/u8g2/master/tools/font/bdf"
 
@@ -20,9 +22,13 @@ defmodule HelloNervesOled.Font do
       raise("font name must end with .bdf")
     end
 
+    ChiselFontCache.get_or_insert_by(bdf_font_name, &build_chisel_font/1)
+  end
+
+  defp build_chisel_font(bdf_font_name) do
     File.mkdir_p(@fonts_destination_dir)
 
-    font_data_src =  Path.join([@fonts_source_base_url, bdf_font_name])
+    font_data_src = Path.join([@fonts_source_base_url, bdf_font_name])
     font_data_file = Path.join([@fonts_destination_dir, bdf_font_name])
 
     if not File.exists?(font_data_file) do
@@ -31,6 +37,7 @@ defmodule HelloNervesOled.Font do
     end
 
     {:ok, %Chisel.Font{} = chisel_font} = Chisel.Font.load(font_data_file)
+
     chisel_font
   end
 end
